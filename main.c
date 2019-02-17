114c921c996f5e3b735a5b9f34f7d1c4dd459086
toasa <atsushi.tohyama.160.333@gmail.com>
2019-02-17 14:27:06 +0900

    add: -, [, ], instructions

--- main.c
+++ main.c
@@ -7,26 +7,71 @@ struct Scan {
 };
 
 char *p;
+char *p_org;
 struct Scan *s;
 
 void init() {
     p = malloc(sizeof(char) * 30000);
 
     s = malloc(sizeof(struct Scan));
-    s->input = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.+.>++++++++++.";
+    //s->input = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.+.>++++++++++.";
+    s->input = "+++++++++[>++++++++>+++++++++++>+++>+<<<<-]>.>++.+++++++..+++.>+++++.<<+++++++++++++++.>.+++.------.--------.>+.>+.";
+    //s->input = "+++[-]";
     s->pos = 0;
 }
 
+void read_char();
+
+int read_until(char c) {
+    s->pos++;
+    while (s->input[s->pos] != c) {
+        read_char();
+        s->pos++;
+    }
+    return s->pos;
+}
+
+int get_pos(char c) {
+    int pos = 0;
+    while (s->input[s->pos + pos] != c) {
+        pos++;
+    }
+    return s->pos + pos;
+}
+
+// for debug
+void check_p() {
+    for (int i = 0; i < 20; i++) {
+        printf("%d ", p_org[i] - '\0');
+    }
+    printf("\n");
+}
+
 void read_char() {
+    // check_p();
+    int src, dst;
     switch (s->input[s->pos]) {
         case '+':
             *p += 1; break;
+        case '-':
+            *p -= 1; break;
         case '.':
             putchar(*p); break;
         case '>':
             p++; break;
         case '<':
             p--; break;
+        case '[':
+            src = s->pos;
+            dst = get_pos(']');
+            while(*p) {
+                dst = read_until(']');
+                s->pos = src;
+            }
+            s->pos = dst;
+            break;
+        case ']':
+            break;
     }
 }
 
@@ -39,6 +84,7 @@ void read() {
 
 int main() {
     init();
+    p_org = p;
     read();
 
     return 0;
